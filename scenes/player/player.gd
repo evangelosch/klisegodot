1e592259c11bdf1e6574f23b3a06f8dff2b2a284
evangelosch <vaggosch@gmail.com>
2023-09-08 20:20:17 +0200

    first commit after godot 4 lesson

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,7 +5,8 @@ const MAX_SPEED = 200
 #dash check
 var dash_direction = Vector2()
 var friction = 0.5
-var dash_speed = 20
+var dash_speed = 10
+var dash_length = 3
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,23 +16,28 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var movement_vector = get_movement_vector()
-	var direction = movement_vector.normalized()
-	velocity = direction * MAX_SPEED
+	#handle movement
+	get_movement_input()
 	move_and_slide()
 	#handle dash
 	if Input.is_action_just_pressed("dash"):
-		dash_direction = direction.normalized()
-		velocity = dash_direction * dash_speed * 10
-		#velocity *= 1.0 - (friction * delta)
+		do_dash(delta)
 		print("dash")
-		move_and_collide(velocity)
-
-
-func get_movement_vector():
-	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	return Vector2(x_movement, y_movement)
 	
-#handle dash
+	
+func get_movement_input():
+	velocity = get_input_direction() * MAX_SPEED
+	
+	
+func do_dash(deltaValue):
+	velocity = get_input_direction() * dash_speed * dash_length
+	velocity *= 1.0 - (friction * deltaValue)
+	move_and_collide(velocity)
+
+
+func get_input_direction():
+	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	if input_direction == null:
+		return Vector2.ZERO
+	return input_direction
 
